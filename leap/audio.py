1733d71bba6ca27c4978a9fdcafbd7de2e9d9a7d
Jacob Geryk <jacobgeryk@gmail.com>
2016-10-08 16:22:43 -0400

    wtf

--- leap/audio.py
+++ leap/audio.py
@@ -6,7 +6,11 @@ import scipy.signal as signal
 import audioop as ap
 from matplotlib import pyplot as plt
 
-
+WIDTH = 2
+CHANNELS = 2
+RATE = 44100
+loop = []
+p = pyaudio.PyAudio()
 
 class audioProcessor:
     def __init__(self):
@@ -20,16 +24,23 @@ class audioProcessor:
         # if reversing:
         #     decoded = ap.reverse(decoded, 4)
         if self.recording:
-            self.loop.append(decoded)
+            loop.append(decoded)
         return decoded
 
     def callback(self, in_data, frame_count, time_info, status):
         global decoded
         global result_waiting
+<<<<<<< HEAD
+=======
+        if(len(loop) > 10000):
+            self.recording = False
+            self.playingBack = True
+>>>>>>> parent of d6a0912... Adding infinite looping
         if in_data and not self.playingBack:
             decoded = self.processSound(in_data)
             result_waiting = True
         elif self.playingBack:
+<<<<<<< HEAD
             if self.loop:
                 if self.loopIndex > len(self.loop) - 1:
                     self.loopIndex = 0
@@ -37,6 +48,9 @@ class audioProcessor:
                 self.loopIndex += 1
             else:
                 self.playingBack = False
+=======
+            decoded = loop.readframes(frame_count)
+>>>>>>> parent of d6a0912... Adding infinite looping
         else:
             print('no input')
         return (decoded, pyaudio.paContinue)
