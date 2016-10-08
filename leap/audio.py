d6a0912d6ff9e8e62137c0d645d106505be38d3b
Jacob Geryk <jacobgeryk@gmail.com>
2016-10-08 14:42:51 -0400

    Adding infinite looping

--- leap/audio.py
+++ leap/audio.py
@@ -9,33 +9,41 @@ from matplotlib import pyplot as plt
 WIDTH = 2
 CHANNELS = 2
 RATE = 44100
-loop = []
 p = pyaudio.PyAudio()
 
 class audioProcessor:
     def __init__(self):
         self.recording = True
         self.playingBack = False
+        self.loop = []
+        self.repeatingLoop = []
 
     def processSound(self, in_data):
         decoded = np.fromstring(in_data, dtype=np.float32)
         # if reversing:
         #     decoded = ap.reverse(decoded, 4)
         if self.recording:
-            loop.append(decoded)
+            self.loop.append(decoded)
         return decoded
 
     def callback(self, in_data, frame_count, time_info, status):
         global decoded
         global result_waiting
-        if(len(loop) > 10000):
+        if(len(self.loop) > 75):
             self.recording = False
             self.playingBack = True
         if in_data and not self.playingBack:
             decoded = self.processSound(in_data)
             result_waiting = True
         elif self.playingBack:
-            decoded = loop.readframes(frame_count)
+            if self.repeatingLoop:
+                decoded = self.repeatingLoop.pop(0)
+                self.loop.append(decoded)
+            elif self.loop:
+                decoded = self.loop.pop(0)
+                self.repeatingLoop.append(decoded)
+            else:
+                self.playingBack = False
         else:
             print('no input')
         return (decoded, pyaudio.paContinue)
