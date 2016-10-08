a4806c9bbfea608e3b09ad49bd4b828fd8e48fb5
Jacob Geryk <jacobgeryk@gmail.com>
2016-10-08 14:10:54 -0400

    Adding class

--- leap/audio.py
+++ leap/audio.py
@@ -6,41 +6,47 @@ import scipy.signal as signal
 import audioop as ap
 from matplotlib import pyplot as plt
 
-
 WIDTH = 2
 CHANNELS = 2
 RATE = 44100
 loop = []
-recording = False
-playingBack = False
 p = pyaudio.PyAudio()
 
-def processSound(in_data):
-    decoded = np.fromstring(in_data, dtype=np.float32)
-    if reversing:
-        decoded = ap.reverse(decoded, 4)
-    if recording:
-        loop.append(decoded)
-    return decoded
+class audioProcessor:
+    def __init__(self):
+        self.recording = True
+        self.playingBack = False
 
-def callback(in_data, frame_count, time_info, status):
-    global decoded
-    global result_waiting
-    if in_data and not playingBack:
-        decoded = processSound(in_data)
-        result_waiting = True
-    else if playingBack:
-        decoded = loop.readframes(frame_count)
-    else:
-        print('no input')
-    return (decoded, pyaudio.paContinue)
+    def processSound(self, in_data):
+        decoded = np.fromstring(in_data, dtype=np.float32)
+        # if reversing:
+        #     decoded = ap.reverse(decoded, 4)
+        if self.recording:
+            loop.append(decoded)
+        return decoded
 
+    def callback(self, in_data, frame_count, time_info, status):
+        global decoded
+        global result_waiting
+        if(len(loop) > 10000):
+            self.recording = False
+            self.playingBack = True
+        if in_data and not self.playingBack:
+            decoded = self.processSound(in_data)
+            result_waiting = True
+        elif self.playingBack:
+            decoded = loop.readframes(frame_count)
+        else:
+            print('no input')
+        return (decoded, pyaudio.paContinue)
+
+ap = audioProcessor()
 stream = p.open(format=p.get_format_from_width(WIDTH),
                 channels=CHANNELS,
                 rate=RATE,
                 input=True,
                 output=True,
-                stream_callback=callback)
+                stream_callback=ap.callback)
 
 stream.start_stream()
 
