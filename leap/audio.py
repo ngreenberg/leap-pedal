72e8870ddd2c237e271aed36e53a279c36eae1e5
Jacob Geryk <jacobgeryk@gmail.com>
2016-10-08 04:24:37 -0400

    updating audio.py

--- leap/audio.py
+++ leap/audio.py
@@ -1,6 +1,11 @@
 
 import pyaudio
 import time
+import numpy as np
+import scipy.signal as signal
+import audioop as ap
+from matplotlib import pyplot as plt
+
 
 WIDTH = 2
 CHANNELS = 2
@@ -9,8 +14,16 @@ RATE = 44100
 p = pyaudio.PyAudio()
 
 def callback(in_data, frame_count, time_info, status):
-    # print status
-    return (in_data, pyaudio.paContinue)
+    global decoded
+    global result_waiting
+    if in_data:
+        decoded = np.fromstring(in_data, dtype=np.float32)
+        # decoded = ap.reverse(decoded, 4)
+        result_waiting = True
+    else:
+        print('no input')
+
+    return (decoded, pyaudio.paContinue)
 
 stream = p.open(format=p.get_format_from_width(WIDTH),
                 channels=CHANNELS,
@@ -23,6 +36,7 @@ stream.start_stream()
 
 while stream.is_active():
     time.sleep(0.1)
+
 
 stream.stop_stream()
 stream.close()
